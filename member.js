02d37c927d256204e597d64480ed02b2fa051676
Faycal Kilali <53886202+faycalki@users.noreply.github.com>
2023-07-19 23:50:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+  var member = this;
+  member.skills = [
+    { name: 'HTML' },
+    { name: 'CSS' },
+    { name: 'JavaScript' },
+    { name: 'AngularJS' },
+    { name: 'NodeJS' },
+    { name: 'MongoDB' }
+  ];
+}
